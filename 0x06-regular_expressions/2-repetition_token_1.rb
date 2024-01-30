f18cbf5fd1c715ec4e384aac5a144d6363021e2b
wallacesintra <wallvxwallace962@gmail.com>
2024-01-30 14:39:38 +0300

    repetition pattern

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-# accepts one argument and pass it to a regular expression matching method
+# accepts one argument and pass it to a regex matching method
 puts.ARGV[0].scan(/hb?tn/).join
