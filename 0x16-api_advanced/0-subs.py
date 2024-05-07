9ed789ca12f1c0e7f66963a7cd09b1ff23fe39f5
wallacesintra <wallvxwallace962@gmail.com>
2024-05-07 21:19:21 +0300

    chore: Update User-Agent header to "Custom" in 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -19,7 +19,7 @@ def number_of_subscribers(subreddit):
 
     req = requests.get(
         "https://www.reddit.com/r/{}/about.json".format(subreddit),
-        headers={"User-Agent": "custom"},
+        headers={"User-Agent": "Custom"},
     )
 
     if req.status_code == 200:
