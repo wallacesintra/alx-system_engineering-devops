e1d5e29359b24871e70f6afb8f0cdb930bbc8213
wallacesintra <wallvxwallace962@gmail.com>
2024-05-07 21:11:52 +0300

    chore: Refactor code to improve readability and maintainability

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
 """
-function that queries the Reddit API and returns the number of subscribers 
-(not active users, total subscribers) for a given subreddit. 
+function that queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit.
 If an invalid subreddit is given, the function should return 0.
 """
 
 
 import requests
+
 
 def number_of_subscribers(subreddit):
     """
